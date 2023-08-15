a7b707b4ebc76f9e97306226d44968bc56bf7658
LiliyaGalina <lilianna.galina@ukr.net>
2023-08-15 15:56:16 +0300

    Minor improvement

--- operators-demo/src/app/components/switch-map-demo/switch-map-demo.component.ts
+++ operators-demo/src/app/components/switch-map-demo/switch-map-demo.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { debounceTime, map, startWith, switchMap, tap } from 'rxjs';
+import { catchError, debounceTime, map, of, shareReplay, startWith, switchMap, tap } from 'rxjs';
 import { IImagesItem } from 'src/app/models/images-envelope.interface';
 import { ImagesLibraryService } from 'src/app/services/images-library.service';
 
@@ -16,27 +16,31 @@ export class SwitchMapDemoComponent {
 
   public form = this.fb.group({
     searchText: this.fb.control<string>(''),
-    debounceTimeInMs: this.fb.nonNullable.control<number>(200),
+    debounceTimeInMs: this.fb.nonNullable.control<number>(50),
   });
 
   public imagesSource$ = this.form.controls['debounceTimeInMs'].valueChanges.pipe(
     // starts its work immediately, does not wait until the control updated
-    startWith(this.form.getRawValue().debounceTimeInMs),
+    startWith(this.form.getRawValue().debounceTimeInMs), // try to remove
     switchMap(debounceTimeInMs => {
       return this.form.controls['searchText'].valueChanges.pipe(
         //startWith?
-        debounceTime(debounceTimeInMs),
+        debounceTime(debounceTimeInMs), // try to remove
         // filter?
         switchMap(searchText => {
           // loading
           return this.imagesLibraryService.search(searchText || '').pipe(
-            map(collection => collection.collection.items),
-            // catcherror
+            map(collection => collection.collection.items.filter(i => !!i.links?.length)),
+            catchError(err => {
+              console.error(err);
+              return of([]);
+            })
           )
         })
       )
     }),
-    tap(console.log)
+    // shares this subscription between consumers
+    shareReplay(1) // try to remove
   );
 
   public elementInDrawer: IImagesItem | null = null;
