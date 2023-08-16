ac0f4251156ab9b489fd4824f149f65411279392
LiliyaGalina <lilianna.galina@ukr.net>
2023-08-16 17:08:48 +0300

    Fetch photos

--- operators-demo/src/app/components/combining-operators/combining-operators.component.ts
+++ operators-demo/src/app/components/combining-operators/combining-operators.component.ts
@@ -1,24 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { interval, scan, shareReplay } from 'rxjs';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {
+  combineLatest,
+  interval,
+  map,
+  scan,
+  shareReplay,
+  switchMap,
+  take,
+  tap,
+} from 'rxjs';
+import { RoverPhotosService } from 'src/app/services/rover-photos.service';
 
 @Component({
   selector: 'app-combining-operators',
   templateUrl: './combining-operators.component.html',
-  styleUrls: ['./combining-operators.component.scss']
+  styleUrls: ['./combining-operators.component.scss'],
 })
-export class CombiningOperatorsComponent implements OnInit {
+export class CombiningOperatorsComponent {
+  private roverPhotosService = inject(RoverPhotosService);
+  private snackBar = inject(MatSnackBar);
 
-  private curiosity$ = interval(867).pipe(shareReplay(1));
-  private opportunity$ = interval(3542).pipe(shareReplay(1));
-  private spirit$ = interval(5443).pipe(shareReplay(1));
+  private curiosity = this.buildRoverChannel('curiosity', 7345, '2016-6-3', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0dBkV46uJkDLYJPMxedDV_Zlm9bsIZlIHQvdd8Adyzk7AgcPeLY5ZLlFqRFlmOp3J5JI&usqp=CAU');
+  private opportunity = this.buildRoverChannel('opportunity', 5234, '2016-6-3', 'https://ychef.files.bbci.co.uk/976x549/p0dh43zd.jpg');
+  private spirit = this.buildRoverChannel('spirit', 1256, '2006-6-3', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/KSC-03PD-0786.jpg/260px-KSC-03PD-0786.jpg');
 
-  public curiosityPhotoStream$ = this.curiosity$.pipe(scan((arr, n) => [...arr, n], [] as any[]));
-  public opportunityPhotoStream$ = this.opportunity$.pipe(scan((arr, n) => [...arr, n], [] as any[]));
-  public spiritPhotoStream$ = this.spirit$.pipe(scan((arr, n) => [...arr, n], [] as any[]));
+  public rovers = [this.curiosity, this.opportunity, this.spirit];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  private buildRoverChannel(name: string, intervalMs: number, dateOfObservation: string, coverImg: string) {
+    const stream$ = this.roverPhotosService
+    .requestPhotosFromRover(name, dateOfObservation)
+    .pipe(
+      switchMap((photos) =>
+        interval(intervalMs).pipe(
+          take(photos.length),
+          map((v, i) => photos[i])
+        )
+      ),
+      shareReplay(1)
+    );
+    const photoStream$ = stream$.pipe(
+      scan((arr, n) => [...arr, n], [] as any[]),
+    )
+    return { name, stream$, photoStream$, coverImg }
   }
 
 }
---
+++ operators-demo/src/app/services/rover-photos.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Constants } from '../classes/constants.class';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoverPhotosService {
+  private http = inject(HttpClient);
+
+  public requestPhotosFromRover(name: string, earthDate: string): Observable<any> {
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/photos?earth_date=${earthDate}&api_key=${Constants.NASA_API_KEY}`;
+    return this.http.get<any>(url).pipe(map(arr => arr.photos.map((x: { img_src: any; }) => x.img_src)));
+  }
+}
